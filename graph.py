bf1282724c2209addb8c8fb616fb389cae639dbe
cyanhudini <n.rudolphi11@gmail.com>
2024-07-24 23:40:29 +0200

    WIP

--- graph.py
+++ graph.py
@@ -1,66 +1,10 @@
-import tkinter
-from matplotlib.figure import Figure 
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,  
-NavigationToolbar2Tk) 
-import random
-from tkinter import filedialog
-import csv
-import numpy as np
-import mpldatacursor
-import mplcursors
+import pandas as pd
 
-root = tkinter.Tk()
-root.title('Data Visualisierung')
-random_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15,16,17,18]
-more = [5,3,2,1,7,86,43,22,6,4,2334,56,3,2,453,65,234,346]
-no = [2,512,7,1,78,3,92,2,6,53, 12,89,45,28,10,15,31,33]
-x1 = np.linspace(0, 10, 100)
-x2 = np.linspace(0, 20, 100)
-y = np.cos(x1)
+# Read the CSV file
+df = pd.read_csv('test.csv')
 
-t = np.arange(0, 3, .01)
-s = np.arange(0, 3, .01)
-u = np.arange(0, 3, .01)
-second = ["a", "b", "c", "d", "e", "f", "g"]
-f = np.arange(7)
-y = np.arange(7)
+# Select rows where the value in the 'column_name' is equal to 'some_value'
+filtered_df = df[df['n'] == 1]
 
-fig, ax = plt.subplots()
-#l, = ax1.plot(x, y, label="")
-plt.xticks(second, f)
-ax.plot(f, y, label="cos")
-ax.xaxis.set_major_locator(plt.MaxNLocator(100))
-#ax2 = ax1.twinx()
-#ax2.plot(x, z)
-#mplcursors.cursor(hover=True)
-canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
-canvas.draw()
-canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
-
-toolbar = NavigationToolbar2Tk(canvas, root)
-toolbar.update()
-canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
-
-
-
-def on_key_press(event):
-    print("you pressed {}".format(event.key))
-    key_press_handler(event, canvas, toolbar)
-
-
-canvas.mpl_connect("key_press_event", on_key_press)
-
-
-def _quit():
-    root.quit()     # stops mainloop
-    root.destroy()  # this is necessary on Windows to prevent
-                    # Fatal Python Error: PyEval_RestoreThread: NULL tstate
-
-
-button = tkinter.Button(master=root, text="Quit", command=_quit)
-button.pack(side=tkinter.BOTTOM)
-
-tkinter.mainloop()
-# If you put root.destroy() here, it will cause an error if the window is
-# closed with the window manager.
+# Or, select rows where the value in 'column_name' is greater than 10
+print(filtered_df)
--- main.py
+++ main.py
@@ -18,13 +18,24 @@ class DataVisualisation:
         self.window.title('Data Visualisierung')
         self.window.grid_rowconfigure(1, weight=1)
         self.window.grid_columnconfigure(0, weight=1)
-        self.imported_headers = []
-        self.plot_data = []
+        self.global_compare_headers = []
         self.grouped_run_trace_ids = [] # gruppierte daten von von traces in jeweils einem run zum vergleichen
+        self.used_headers_x = []
+        self.plot_data = []
         self.window_width = 2500
         self.window_height = 1300
         self.window.geometry("2500x1300") 
         self.coloumn_counter=0        
+            
+        self.selected_x1 = StringVar()
+        self.selected_x2 = StringVar()
+        self.selected_y1 = StringVar()
+        self.selected_ys = []
+        self.selected_xs = []
+        # label text comapre button
+        self.compare_button_text = StringVar(value="Compare")
+        # flag to indicate which text to display
+        self.compare_flag = 1
         
         self.tooltip = mplcursors.cursor(hover=True)
         # top und bottom frame
@@ -45,62 +56,108 @@ class DataVisualisation:
         self.graph_frame.grid(row=0, column=0, sticky="nsew")
         
         # buttons
-        self.plot_button = Button(master = self.button_bar_frame,command = self.plot,height = 2,width = 10,text = "Plot").grid(row=0, column=0)
-        self.upload_button = Button(master = self.button_bar_frame,command = self.upload_dataset, height = 2, width = 10, text = "Upload").grid(row=0, column=1)
-        self.clear_button = Button(master = self.button_bar_frame, command = self.clear, height = 2, width = 10, text = "clear").grid(row=0, column=2)
-        self.clear_all_button = Button(master = self.button_bar_frame, command = self.clear_all, height = 2, width = 10, text = "clear all", background = "red").grid(row=0, column=3)
+        self.plot_button = Button(master = self.button_bar_frame,command = self.plot,height = 2,width = 10,text = "Plot")
+        self.plot_button.grid(row=0, column=0)
+        self.upload_button = Button(master = self.button_bar_frame,command = self.upload_dataset, height = 2, width = 10, text = "Upload")
+        self.upload_button.grid(row=0, column=1)
+        self.clear_button = Button(master = self.button_bar_frame, command = self.clear, height = 2, width = 10, text = "clear")
+        self.clear_button.grid(row=0, column=2)
+        self.clear_all_button = Button(master = self.button_bar_frame, command = self.clear_all, height = 2, width = 10, text = "clear all", background = "red")
+        self.clear_all_button.grid(row=0, column=3)
         #self.add_sec_axis_button = Button(master = self.button_bar_frame, command = self.add_sec_axis, height = 2, width = 10, text = "2. Achse").grid(row=0, column=5)
-        self.add_sec_y_axis_button = Button(master = self.button_bar_frame, command = self.add_more_y_against_x1, height = 2, width = 10, text = "X(Y_c)").grid(row=0, column=5)
-        self.to_compare_runs = Button(master = self.button_bar_frame, command = self.compare_runs, height = 2, width = 10, text = "Compare").grid(row=0, column=6)
-        self.to_switch_comparison_scope = Button(master = self.button_bar_frame, command = self.switch_between_global_compare_and_single_runs, height = 2, width = 10, text = "Switch").grid(row=0, column=7)
+        self.add_more_axis_button = Button(master = self.button_bar_frame, command = self.add_more_axis, height = 2, width = 10, text = "Add Axis").grid(row=0, column=5)
+        self.to_compare_runs_button = Button(master = self.button_bar_frame, command = self.switch_compare_scope, height = 2, width = 10, text = self.compare_button_text.get())
+        self.to_compare_runs_button.grid(row=0, column=6)
         
-        self.selected_x = StringVar()
-        self.selected_y1 = StringVar()
-        self.selected_ys = []
+        
+        
+        
     # TODO: switch_between_global_compare_and_single_runs, re-import headers von csv_df/csv file
     
     
-    def compare_runs(self):
-        # coloumns werden "gruppiert" indem duplikate entfernt werden
-        print("compare" )
-        to_group = self.csv_df["name"]
-        for i in to_group:
-            if i not in self.grouped_run_trace_ids:
-                print(i)
-                self.grouped_run_trace_ids.append(i)
-    
-    def switch_between_global_compare_and_single_runs(self):
-        self.clear_all()
-        pass
-    
-    def import_headers_from_csv(self):
+    def switch_compare_scope(self):
         
-    def add_more_y_against_x1(self):
+        print("compare")
+        self.clear()
+        if self.compare_flag == 0:
+            self.used_headers_x = []
+            self.used_headers_x = self.global_compare_headers
+            self.change_optionsmenu_options()
+            self.compare_button_text.set("Compare")
+            self.to_compare_runs_button.config(text=self.compare_button_text.get())
+            self.compare_flag = 1
+
+        else:
+            self.used_headers_x = []
+            self.used_headers_x = self.grouped_run_trace_ids
+            self.change_optionsmenu_options()
+            self.compare_button_text.set("Global")
+            self.to_compare_runs_button.config(text=self.compare_button_text.get())
+            self.x1_label.config(text="Run ID")
+            self.dropdown_x2_for_run_compare = OptionMenu(self.button_bar_frame, self.selected_x2, *self.global_compare_headers)
+            self.dropdown_x2_for_run_compare.grid(row=1, column=self.coloumn_counter)
+            self.coloumn_counter+=1
+            self.dropdown_x2_label = Label(master = self.button_bar_frame, text = "X-Achse")
+            self.dropdown_x2_label.grid(row=1, column=self.coloumn_counter+1)
+            self.coloumn_counter+=1
+            self.compare_flag = 0
+            
+            
+    def change_optionsmenu_options(self):
+        menu = self.dropdown_x1["menu"]
+        menu.delete(0, "end")
+        for child in self.button_bar_frame.winfo_children():
+            if isinstance(child, OptionMenu):
+                for options in self.used_headers_x:
+                    menu.add_command(label=options, command=lambda value=options: self.selected_x1.set(value))
+    
+    def add_more_axis(self):
         selected_y = StringVar()
-        self.dropdown_y = OptionMenu(self.button_bar_frame, selected_y , *self.imported_headers).grid(row=1, column=self.coloumn_counter)
+        selected_x1 = StringVar()
+        
+        dropdown_x = OptionMenu(self.button_bar_frame, selected_x1 , *self.used_headers_x)
+        dropdown_x.grid(row=1, column=self.coloumn_counter)
+        
+        self.coloumn_counter+=1
+        
+        dropdown_y = OptionMenu(self.button_bar_frame, selected_y , *self.global_compare_headers)
+        dropdown_y.grid(row=1, column=self.coloumn_counter)
         self.selected_ys.append(selected_y)
         self.coloumn_counter+=1
     
     def plot(self): 
         self.clear()
-        if not self.selected_x or not self.selected_y1:
+        if not self.selected_x1 or not self.selected_y1:
             print("Bitte wählen Sie die Achsen")
             return
         
        
-        x1 = (self.csv_df[self.selected_x.get()])
-        #y1 = (self.csv_df[self.selected_y1.get()])
+        
         self.fig, self.ax1 = plt.subplots()
         self.ax1.xaxis.set_major_locator(plt.MaxNLocator(10))
         
-        plt.xticks(rotation=35, ha='right')  
-        for y in self.selected_ys:
-            y = (self.csv_df[y.get()])
-            self.ax1.plot(x1, y,"-r", label="ax1")
+        if self.compare_flag == 1:
+            for current_x in self.selected_xs:
+                for current_y in self.selected_ys:
+                    filtered_data = self.csv_df[self.csv_df['name'] == self.selected_x1.get()]
+                    x = (filtered_data[self.current_x.get()])
+                    print(x.to_list())
+                    print(self.selected_x1.get())
+                    
+                    y = pd.to_numeric(filtered_data[self.current_y.get()])
+                    self.ax1.plot(x, y,"-r", label="ax1")
+        
+        else:
+            x1 = (self.csv_df[self.selected_x1.get()])
+            #y1 = (self.csv_df[self.selected_y1.get()])
+            for y in self.selected_ys:
+                y = (self.csv_df[y.get()])
+                self.ax1.plot(x1, y,"-r", label="ax1")
+        plt.xticks(rotation=35, ha='right') 
        
         
         plt.legend()
-        self.fig.set_size_inches(25,11.5)
+        self.fig.set_size_inches(20,9.5)
         self.canvas = FigureCanvasTkAgg(self.fig, master =self.graph_frame)
         self.canvas.draw()
         #self.canvas.get_tk_widget()
@@ -120,34 +177,44 @@ class DataVisualisation:
             with(open(csv_file, "r")) as file:
                 
                 csv_reader = csv.reader(file)
-                self.imported_headers = next(csv_reader) 
-                                
+                self.global_compare_headers = next(csv_reader)
+                self.used_headers_x = self.global_compare_headers
                 for row in csv_reader:
                     int_row = [(item) for item in row]
                     self.plot_data.append(row)
         
-        self.compare_runs()
-        self.dropdown_x1 = OptionMenu(self.button_bar_frame, self.selected_x, *self.imported_headers).grid(row=1, column=self.coloumn_counter)
+        to_group = self.csv_df["name"]
+        for i in to_group:
+                if i not in self.grouped_run_trace_ids:
+                    print(i)
+                    self.grouped_run_trace_ids.append(i)
         
+        #self.compare_runs()
+        # selected_x1 entweder auswahl von globalen headers oder von grouped_run_trace_ids
+        self.dropdown_x1 = OptionMenu(self.button_bar_frame, self.selected_x1, *self.used_headers_x)
+        self.dropdown_x1.grid(row=1, column=self.coloumn_counter)
+        self.selected_xs.append(self.selected_x1)
         self.coloumn_counter+=1
         #internal_x1_coloumn_counter = self.coloumn_counter +1
         
-        self.x1_label = Label(master = self.button_bar_frame, text = "X-Achse").grid(row=1, column=self.coloumn_counter)
-        
+        self.x1_label = Label(master = self.button_bar_frame, text = "X-Achse")
+        self.x1_label.grid(row=1, column=self.coloumn_counter)
         self.coloumn_counter+=1
         
-        self.dropdown_y1 = OptionMenu(self.button_bar_frame, self.selected_y1, *self.imported_headers).grid(row=1, column=self.coloumn_counter)
+        self.dropdown_y1 = OptionMenu(self.button_bar_frame, self.selected_y1, *self.used_headers_x)
+        self.dropdown_y1.grid(row=1, column=self.coloumn_counter)
         self.selected_ys.append(self.selected_y1)
-        
         self.coloumn_counter+=1
         
-        self.y1_label = Label(master = self.button_bar_frame, text = "X(Y1)-Achse").grid(row=1, column=self.coloumn_counter)
+        self.y1_label = Label(master = self.button_bar_frame, text = "Y-Achse")
+        self.y1_label.grid(row=1, column=self.coloumn_counter)
         
         self.coloumn_counter+=1
         
     def clear(self):
         print("clear")
-
+        self.compare_flag = 1
+        self.selected_ys = []
         if self.canvas:
             for child in self.graph_frame.winfo_children():
                 if isinstance(child, Canvas):
@@ -157,7 +224,6 @@ class DataVisualisation:
     def clear_all(self):
 
         self.clear()
-        
         self.imported_headers = []
         self.plot_data = []
         for child in self.button_bar_frame.winfo_children():
